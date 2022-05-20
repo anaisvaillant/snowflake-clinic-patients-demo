782844e5e26f3c21a8567cf8d624489b18e17abb
Matt Cutini <matt.cutini@fishtownanalytics.com>
2022-05-20 17:53:58 +0000

    Creates the staging models for patients, encounters, and conditions

---
+++ models/staging/stg_conditions.sql
@@ -0,0 +1,8 @@
+select 
+  "START" as start_date
+, "STOP" as end_date
+, patient as patient_id
+, encounter as encounter_id
+, code as condition_code
+, "DESCRIPTION" as condition_description
+from {{ source('clinic', 'conditions') }}
---
+++ models/staging/stg_patients.sql
@@ -0,0 +1,15 @@
+select
+  id as patient_id
+, birthdate as birth_date
+, deathdate as death_date
+, ssn
+, drivers as driver_license
+, passport
+, prefix
+, regexp_substr(first, '[A-Za-z]*') as first_name
+, regexp_substr(last, '[A-Za-z]*') as last_name
+, marital as marital_status
+, race
+, ethnicity
+, gender
+from {{ source('clinic', 'patients') }}
