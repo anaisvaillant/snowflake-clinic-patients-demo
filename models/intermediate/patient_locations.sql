55f58afdc4bb146d7fdc308271a1ee16a949a215
Matt Cutini <matt.cutini@fishtownanalytics.com>
2022-09-19 17:53:31 +0000

    Updates the patient_locations model

---
+++ models/intermediate/patient_locations.sql
@@ -0,0 +1,8 @@
+select 
+  p.first_name,
+  p.last_name,
+  p.zip,
+  z.name as city_name
+from {{ ref('stg_patients') }} as p
+join {{ ref('Utah_ZIP_Code_Areas') }} as z
+  on p.zip = z.zip5
--- models/staging/clinic/stg_patients.sql
+++ models/staging/clinic/stg_patients.sql
@@ -11,6 +11,6 @@ marital as marital_status,
 race,
 ethnicity,
 gender,
-zip
+zip,
 current_timestamp as last_updated_dts
 FROM {{ source('clinic', 'patients') }}
